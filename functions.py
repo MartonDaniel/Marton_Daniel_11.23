be39e55df59cc83b974208d1970df26ace13d1fa
09D_Marton Dániel <marton.daniel@students.jedlik.eu>
2022-11-28 10:53:02 +0100

    1. menüpot

--- functions.py
+++ functions.py
@@ -11,3 +11,13 @@ def menu():
     print('4 - könyv visszahozása a könyvtárba')
     print('5 - könyv törlése a listából')
     return input('Választás: ')
+
+def konyveklistaja():
+    file=open('Books.csv','r',encoding="utf-8")
+    file.readline()
+    for egysor in file:
+        darabolt=egysor.strip().split(',')
+    cim.append(darabolt[0])
+    szerzö.append(float(darabolt[1]))
+    megtalalhato.append(float(darabolt[2]))
+    file.close()
---
+++ main.py
@@ -0,0 +1,7 @@
+from functions import *
+from os import system
+valasztas=""
+while valasztas!='0':
+    valasztas=menu()
+    if valasztas=="1":
+        konyveklistaja()
